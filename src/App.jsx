801e98778523a641c9a29bd15664f4b2491b7092
Iheb Ouelhazi <ouelhazi3iheb@gmail.com>
2022-08-27 23:37:50 +0100

    Add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>give feedbac</h1>
+      <Button text="good" />
+      <Button text="neutral" />
+      <Button text="bad" />
+      <h1>statistics</h1>
+      <Display text="good" score={good} />
+      <Display text="neutral" score={neutral} />
+      <Display text="bad" score={bad} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,3 @@
+export default function Button(props) {
+  <button onClick={props.handleClick}>{props.text}</button>;
+}
